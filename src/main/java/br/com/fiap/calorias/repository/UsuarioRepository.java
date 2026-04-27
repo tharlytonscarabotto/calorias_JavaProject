268a626545985b537f88ee5e840610783192c610
Tharlyton Scarabotto <tharlyton.scarabotto@outlook.com>
2026-04-26 22:15:04 -0300

    Criação das camadas para a classe Usuário e criação das tabelas no banco de dados utilizando FlyWay

---
+++ src/main/java/br/com/fiap/calorias/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.calorias.repository;
+
+import br.com.fiap.calorias.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
